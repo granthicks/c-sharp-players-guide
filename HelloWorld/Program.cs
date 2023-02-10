edb559e5933fae647aa998460f94ce82bfefc1fb
Grant Hicks <grant.hicks00@gmail.com>
2023-02-10 14:16:38 -0500

    initial commit

---
+++ HelloWorld/Program.cs
@@ -0,0 +1,6 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Here is line 1");
+Console.WriteLine("Here is line 2");
+Console.WriteLine("Here is line 3");
+Console.WriteLine("Here is line 4");
+Console.WriteLine("Here is line 5");
