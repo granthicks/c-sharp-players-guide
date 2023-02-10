821f9ad21ac3631aafa27fc261af69434e989e5d
Grant Hicks <grant.hicks00@gmail.com>
2023-02-10 14:39:40 -0500

    added The Thing Namer 3000

---
+++ ThingNamer3000/Program.cs
@@ -0,0 +1,13 @@
+﻿// See https://aka.ms/new-console-template for more information
+/* The Thing Namer 3000 */
+// Prompt user for the king of thing to name
+Console.WriteLine("What kind of thing are we talking about?");
+string a = Console.ReadLine();
+// Prompt user for general description of the thing
+Console.WriteLine("How would you describe it? Big? Azure? Tattered?");
+string b = Console.ReadLine();
+// Add "of Doom" to the name of the thing
+string c = "of Doom";
+// Tack 3000 on the end to sound fancy.
+string d = "3000";
+Console.WriteLine("The " + b + " " + a + " " + c + " " + d + "!");
