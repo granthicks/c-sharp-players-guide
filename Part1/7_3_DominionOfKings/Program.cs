60d5eed112081aad799010d39dd93e55e2a25754
Grant Hicks <grant.hicks00@gmail.com>
2023-02-11 23:18:05 -0500

    added dominion of kings

---
+++ Part1/7_3_DominionOfKings/Program.cs
@@ -0,0 +1,23 @@
+﻿/* The Dominion of Kings
+ * Calculates a total score to rank a kingdom.
+ * Takes input of provinces, duchies, and estates a king has.
+ * Estate = 1 point
+ * Duchy = 3 points
+ * Province = 6 points */
+
+Console.WriteLine("How many estates does the king have?");
+string str_estates = Console.ReadLine();
+int estates = Convert.ToInt32(str_estates);
+
+Console.WriteLine("How many duchies does the king have?");
+string str_duchies = Console.ReadLine();
+int duchies = Convert.ToInt32(str_duchies);
+int duchies_score = duchies *= 3;
+
+Console.WriteLine("How many Provinces does the king have?");
+string str_provinces = Console.ReadLine();
+int provinces = Convert.ToInt32(str_provinces);
+int provinces_score = provinces *= 6;
+
+int total_score = estates + duchies_score + provinces_score;
+Console.WriteLine("The total score for the kingdom is " + total_score);
