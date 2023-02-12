24ef0f8efe64bd4b88de7c37a53006226fc32bbc
Grant Hicks <grant.hicks00@gmail.com>
2023-02-11 22:34:14 -0500

    added the triange farmer

---
+++ TheTriangeFarmer/Program.cs
@@ -0,0 +1,12 @@
+﻿/* The Triangle Farmer
+ * Takes input of triange base size and height
+ * Computes the area of the triange and writes the result. */
+
+Console.WriteLine("Please enter the base of the triange");
+string str_b = Console.ReadLine();
+double b = Convert.ToDouble(str_b);
+Console.WriteLine("Please enter the height of the triange");
+string str_h = Console.ReadLine();
+double h = Convert.ToDouble(str_h);
+double a = (b * h) / 2;
+Console.WriteLine("The area of your triange is " + a);
