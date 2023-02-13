473aac38adc79bfa0bafb830da312bb8c2dd7858
Grant Hicks <grant.hicks00@gmail.com>
2023-02-13 11:38:01 -0500

    added repairing the clocktower

---
+++ Part1/9_1_RepairingTheClocktower/Program.cs
@@ -0,0 +1,18 @@
+﻿/* Repairing the Clocktower
+ * Take a number as input
+ * Display the word 'tick' if number is even
+ * Display the word 'tock' if number is odd */
+
+Console.Title = "Repairing the Clocktower";
+
+Console.WriteLine("Please input a number");
+string user_input = Console.ReadLine();
+
+int num = Convert.ToInt32(user_input);
+
+if (num % 2 == 0)
+    Console.WriteLine("Tick");
+else if (num % 2 != 0)
+    Console.WriteLine("Tock");
+else
+    Console.WriteLine("Something went wrong!");
