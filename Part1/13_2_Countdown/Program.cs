f8b1d5a76769e066697e70d3a6fad5edeffabcab
Grant Hicks <grant.hicks00@gmail.com>
2023-02-23 17:05:38 -0500

    Added Countdown

---
+++ Part1/13_2_Countdown/Program.cs
@@ -0,0 +1,17 @@
+﻿/* Countdown
+ * Write code that counts down from 10 to 1 using a recursive method */
+
+Console.Title = "Countdown";
+
+int Countdown(int number)
+{
+    if (number == 1) return 1;
+    else
+    {
+        Console.WriteLine(number);
+        return Countdown(number - 1);
+    }
+}
+
+// Testing with 10
+Console.WriteLine(Countdown(10));
