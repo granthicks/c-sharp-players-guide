2ccfc1192b7db46a54db9ace2d0d81d4081e164f
Grant Hicks <grant.hicks00@gmail.com>
2023-04-05 19:18:09 -0400

    completed TheLockedDoor

--- Part2/24_4_TheLockedDoor/Program.cs
+++ Part2/24_4_TheLockedDoor/Program.cs
@@ -12,9 +12,62 @@
 
 Console.Title = "The Locked Door";
 
+Console.Write("Enter initial passcode: ");
+int initialCode = Convert.ToInt32(Console.ReadLine());
+Door door = new Door(initialCode);
+
+while (true)
+{
+  Console.WriteLine($"The door is currently {door.State}.");
+  Console.WriteLine("What would you like to do?");
+  string input = Console.ReadLine();
+  switch (input)
+  {
+    case "open":
+      door.Open();
+      break;
+    case "close":
+      door.Close();
+      break;
+    case "lock":
+      door.Lock();
+      break;
+    case "unlock":
+      Console.Write("Enter passcode: ");
+      int code = Convert.ToInt32(Console.ReadLine());
+      door.Unlock(code);
+      break;
+    case "change code":
+      Console.Write("Enter current passcode: ");
+      int current = Convert.ToInt32(Console.ReadLine());
+      Console.Write("Enter new passcode: ");
+      int newCode = Convert.ToInt32(Console.ReadLine());
+      bool success = door.ChangePasscode(current, newCode);
+      if (success)
+      {
+        Console.WriteLine("The passcode has been changed.");
+      }
+      else
+      {
+        Console.WriteLine("Current passcode incorrect. The passcode has not been changed.");
+      }
+      break;
+    default:
+      Console.WriteLine("Sorry, I don't know what that means. Try again.");
+      break;
+  }
+}
+
 public class Door
 {
+  private int _currentCode;
+
   public DoorState State { get; set; } = DoorState.Closed;
+  
+  public Door(int initialCode)
+  {
+    _currentCode = initialCode;
+  }
 
   public void Open()
   {
@@ -26,16 +79,37 @@ public class Door
 
   public void Close()
   {
-    if (State = DoorState.Open)
+    if (State == DoorState.Open)
     {
       State = DoorState.Closed;
     }
   }
-}
-
-public void Lock()
-{
   
+  public void Lock()
+  {
+    if (State == DoorState.Closed)
+    {
+      State = DoorState.Locked;
+    }
+  }
+
+  public void Unlock(int code)
+  {
+    if (State == DoorState.Locked && code == _currentCode)
+    {
+      State = DoorState.Closed;
+    }
+  }
+
+  public bool ChangePasscode(int currentCode, int newCode)
+  {
+    if (currentCode == _currentCode)
+    {
+      _currentCode = newCode;
+      return true;
+    }
+    return false;
+  }
 }
 
 public enum DoorState { Locked, Open, Closed }
