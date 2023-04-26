d2e6d5d8f269fa16a0761b49c266994324677d2b
Grant Hicks <grant.hicks00@gmail.com>
2023-04-26 17:57:01 -0400

    started the old robot

---
+++ Part2/26_2_TheOldRobot/Program.cs
@@ -0,0 +1,20 @@
+﻿public class Robot
+{
+ public int X { get; set; }
+ public int Y { get; set; }
+ public bool IsPowered { get; set; }
+ public RobotCommand?[] Commands { get; } = new RobotCommand?[3];
+ public void Run()
+ {
+ foreach (RobotCommand? command in Commands)
+ {
+ command?.Run(this);
+ Console.WriteLine($"[{X} {Y} {IsPowered}]");
+ }
+ }
+}
+
+public class RobotCommand
+{
+    public abstract void Run(Robot robot);
+}
