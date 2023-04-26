80d4c7103bfc4f9b7a4f67e427932990ce28dc13
Grant Hicks <grant.hicks00@gmail.com>
2023-04-26 11:56:49 -0400

    Added PackingInventory

--- Part2/26_1_PackingInventory/Program.cs
+++ Part2/26_1_PackingInventory/Program.cs
@@ -2,6 +2,9 @@
  * 
  * Create an InventoryItem class that represents any of the different item types. 
  *      This class must represent the item's weight and volume, which it needs at creation time (constructor).
+ * (1) An arrow has a weight of 0.1 and a volume of 0.05. (2) A bow has a weight of 1 and a volume 
+ * of 4.(3) Rope has a weight of 1 and a volume of 1.5.(4) Water has a weight of 2 and a volume of 3. (5) Food 
+ * rations have a weight of 1 and a volume of 0.5. (6) A sword has a weight of 5 and a volume of 3.
  * Create derived classes for each of they types of items above.
  *      Each class should pass the correct weight and volume to the base class constructor but should be createable 
  *      themselves with a parameterless constructor.
@@ -17,118 +20,102 @@
 
 Console.Title = "Packing Inventory";
 
+Pack pack = new Pack(10, 20, 30);
+
+while (true)
+{
+    Console.WriteLine($"Pack is currently at {pack.CurrentCount}/{pack.MaxCount} items, {pack.CurrentWeight}/{pack.MaxWeight} weight, and {pack.CurrentVolume}/{pack.MaxVolume} volume.");
+
+    Console.WriteLine("What do you want to add?");
+    Console.WriteLine("1 - Arrow");
+    Console.WriteLine("2 - Bow");
+    Console.WriteLine("3 - Rope");
+    Console.WriteLine("4 - Water");
+    Console.WriteLine("5 - Food");
+    Console.WriteLine("6 - Sword");
+    Console.WriteLine("0 - Exit");
+
+    Console.Write("Choice: ");
+    int choice = Convert.ToInt32(Console.ReadLine());
+
+    Console.Clear();
+
+    if (choice == 0) 
+    {
+        Console.WriteLine("Goodbye.");
+        break;
+    }
+
+    if (choice < 0 || choice > 6)
+    {
+        Console.WriteLine("Invalid choice.");
+        continue;
+    }
+
+    InventoryItem newItem = choice switch
+    {
+        1 => new Arrow(),
+        2 => new Bow(),
+        3 => new Rope(),
+        4 => new Water(),
+        5 => new Food(),
+        6 => new Sword()
+    };
+
+    if (!pack.Add(newItem))
+        Console.WriteLine("Could not add this to the pack.");
+}
+
+public class Pack
+{
+    public int MaxCount { get; }
+    public float MaxVolume { get; }
+    public float MaxWeight { get; }
+
+    private InventoryItem[] _items;
+    
+    public int CurrentCount { get; private set; }
+    public float CurrentVolume { get; private set; }
+    public float CurrentWeight { get; private set; }
+
+    public Pack(int maxCount, float maxVolume, float maxWeight)
+    {
+        MaxCount = maxCount;
+        MaxVolume = maxVolume;
+        MaxWeight = maxWeight;
+
+        _items = new InventoryItem[maxCount];
+    }
+
+    public bool Add(InventoryItem item)
+    {
+        if (CurrentCount >= MaxCount) return false;
+        if (CurrentVolume + item.Volume > MaxVolume) return false;
+        if (CurrentWeight + item.Weight > MaxWeight) return false;
+
+        _items[CurrentCount] = item;
+        CurrentCount++;
+        CurrentVolume += item.Volume;
+        CurrentWeight += item.Weight;
+        return true;
+    }
+}
+
 public class InventoryItem
 {
-    public double Weight { get; }
-    public double Volume { get; }
+    public float Weight { get; }
+    public float Volume { get; }
 
-    public InventoryItem(double weight, double volume)
+    public InventoryItem(float weight, float volume)
     {
         Weight = weight;
         Volume = volume;
     }
 }
 
-public class Arrow : InventoryItem
-{
-    public Arrow() : base(0.1, 0.05)
-    {
-
-    }
-}
-
-public class Bow : InventoryItem
-{
-    public Bow() : base(1, 4)
-    {
-
-    }
-}
-
-public class Rope : InventoryItem
-{
-    public Rope(): base(1, 1.5)
-    {
-
-    }
-}
-
-public class Water : InventoryItem
-{
-    public Water() : base(2, 3)
-    {
-
-    }
-}
-
-public class Rations : InventoryItem
-{
-    public Rations() : base(1, 0.5)
-    {
-
-    }
-}
-
-public class Sword : InventoryItem
-{
-    public Sword() : base(5, 3)
-    {
-
-    }
-}
-
-public class Pack
-{
-    private InventoryItem[] items;
-    private int maxItems;
-    private double maxWeight;
-    private double maxVolume;
-    private int itemCount;
-    private double currentWeight;
-    private double currentVolume;
-
-    public Pack(int maxItems, double maxWeight, double maxVolume)
-    {
-        this.maxItems = maxItems;
-        this.maxWeight = maxWeight;
-        this.maxVolume = maxVolume;
-        items = new InventoryItem[maxItems];
-    }
-
-    public bool Add(InventoryItem item)
-    {
-        if (itemCount >= maxItems || currentWeight + item.Weight > maxWeight || currentVolume + item.Volume > maxVolume)
-        {
-            return false;
-        }
-        else
-        {
-            items[itemCount] = item;
-            itemCount++;
-            currentWeight += item.Weight;
-            currentVolume += item.Volume;
-            return true;
-        }
-    }
-
-    public int ItemCount => itemCount;
-
-    public double CurrentWeight => currentWeight;
-
-    public double CurrentVolume => currentVolume;
-
-    public int MaxItems => maxItems;
-
-    public double MaxWeight => maxWeight;
-
-    public double  MaxVolume => maxVolume;
-}
-
-public class Program
-{
-    static void Main(string[] args)
-    {
-        Pack pack = new Pack()
-}
-
-
+public class Arrow : InventoryItem { public Arrow() : base(0.1f, 0.05f) { } }
+public class Bow : InventoryItem { public Bow() : base(1, 4) { } }
+public class Rope : InventoryItem { public Rope() : base(1, 1.5f) { } }
+public class Water : InventoryItem { public Water() : base(2, 3) { } }
+public class Food : InventoryItem { public Food() : base(1, 0.5f) { } }
+public class Sword : InventoryItem { public Sword() : base(5, 3) { } }
