661e064006cc1042e5081003f196c2b6c31a938c
Grant Hicks <grant.hicks00@gmail.com>
2023-04-27 17:57:29 -0400

    worked on old robot

--- Part2/26_2_TheOldRobot/Program.cs
+++ Part2/26_2_TheOldRobot/Program.cs
@@ -18,3 +18,75 @@ public class RobotCommand
 {
     public abstract void Run(Robot robot);
 }
+
+public class OnCommand : RobotCommand
+{
+    public override void Run(Robot robot)
+    {
+        robot.IsPowered = true;
+    }
+}
+
+public class OffCommand : RobotCommand
+    {
+    public override void Run(Robot robot)
+    {
+        robot.IsPowered = false;
+    }
+}
+
+public class NorthCommand
+    {
+    public override void Run(Robot robot)
+    {
+        if (robot.IsPowered)
+        {
+            robot.Y++;
+        }
+    }
+}
+
+public class SouthCommand
+    {
+    public override void Run(Robot robot)
+    {
+        if (robot.IsPowered)
+        {
+            robot.Y--;
+        }
+    }
+}
+
+public class EastCommand
+    {
+    public override void Run(Robot robot)
+    {
+        if (robot.IsPowered)
+        {
+            robot.X++;
+        }
+    }
+}
+
+public class WestCommand
+    {
+    public override void Run(Robot robot)
+    {
+        if (robot.IsPowered)
+        {
+            robot.X--;
+        }
+    }
+}
+
+public class Program
+    {
+    public static void Main()
+    {
+    Robot robot = new Robot();
+    robot.Commands[0] = new OnCommand();
+    robot.Commands[1] = new NorthCommand();
+    robot.Commands[2] = new OffCommand();
+    robot.Run();
+    }
+}
