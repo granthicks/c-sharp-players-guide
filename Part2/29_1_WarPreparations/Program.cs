7ca3b12c7c18deb0eead25f3d537c5f1f3247d29
granthicks <grant.hicks00@gmail.com>
2023-05-17 19:35:50 -0400

    Added War Preparations

--- Part2/29_1_WarPreparations/Program.cs
+++ Part2/29_1_WarPreparations/Program.cs
@@ -1,1 +1,40 @@
-﻿
+﻿/* War Preparations
+*
+* - Swords can be made out of the following materials: bronze, iron, steel, and the rare binarium. Create an enumeration to represent the material type.
+* - Gemstones can be attached to a sword, which gives them strange powers through Cygnus and Lyra’s touch. Gemstone types include emerald, amber, sapphire, diamond, and the rare bitstone. Or no gemstone at all. Create an enumeration to represent a gemstone type.
+* - Create a Sword record with a material, gemstone, length, and crossguard width.
+* - In your main program, create a basic Sword instance made out of iron and with no gemstone. Then
+*   create two variations on the basic sword using with expressions.
+* - Display all three sword instances with code like Console.WriteLine(original);
+*/
+
+Console.Title = "War Preparations";
+
+Sword original = new Sword(MaterialType.Iron, GemstoneType.None, 1.2, 0.2);
+Sword withMaterial = original with { Material = MaterialType.Binarium };
+Sword withGemstone = original with { Gemstone = GemstoneType.Bitstone };
+
+Console.WriteLine("War Preparations: Swords");
+Console.WriteLine(original);
+Console.WriteLine(withMaterial);
+Console.WriteLine(withGemstone);
+
+public record Sword(MaterialType Material, GemstoneType Gemstone, double Length, double CrossguardWidth);
+
+public enum MaterialType
+{
+    Bronze,
+    Iron,
+    Steel,
+    Binarium
+}
+
+public enum GemstoneType
+{
+    Emerald,
+    Amber,
+    Sapphire,
+    Diamond,
+    Bitstone,
+    None
+}
